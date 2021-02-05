1aef7c72a2decdf49091b3ceb321fda38bc3a63d
Maurice Rosenbaum <rose.tho95@outlook.com>
2021-02-05 08:32:10 +0100

    Startversion

---
+++ K10U3_FahrzeugBewegen_MauriceR/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace K10U3_FahrzeugBewegen_MauriceR
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
